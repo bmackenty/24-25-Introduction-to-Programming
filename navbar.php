7d1672005f7ea8fb7db1180c5f087480da877cf4
Bill MacKenty <bmackenty@users.noreply.github.com>
2024-09-18 15:13:56 +0200

    Create navbar.php

---
+++ navbar.php
@@ -0,0 +1,17 @@
+<nav>
+            <!-- 
+            The <ul> tag defines an unordered list. The list items (<li>) inside are the navigation links.
+            -->
+            <ul>
+                <!-- 
+                Each <li> tag represents an item in the list. Inside each list item, there is an <a> tag, 
+                which creates a hyperlink. The href attribute specifies the destination of the link.
+                In this case, they are placeholders with "#" because no actual page is linked.
+                -->
+                <li><a href="default_template.php">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="login.php">Login</a></li>
+            </ul>
+        </nav>
