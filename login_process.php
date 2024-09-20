2bf9ae8f91d7145af19e4146a1ca054f86dd3ac7
Bill MacKenty <bmackenty@users.noreply.github.com>
2024-09-20 08:02:01 +0200

    Update login_process.php

--- login_process.php
+++ login_process.php
@@ -8,7 +8,7 @@ $authorized_user = "test_user";
 
 if($password == $secret_password){
     echo "Welcome $username";
-} elseif ($allowed_user != $authorized_user) {
+} elseif ($username != $authorized_user) {
     echo "Error: This user is not authorized";
 } else {
     echo "Error: this user's password is not correct.";
