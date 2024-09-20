32d4f95f59584f8e413dd9620aeda890459593ad
Bill MacKenty <bmackenty@users.noreply.github.com>
2024-09-20 08:33:27 +0200

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,7 @@
+        <!-- 
+        The <footer> tag defines a footer for the webpage. It typically contains information about the website or contact details.
+        -->
+
+        <footer>
+            <p>Footer content</p>
+        </footer>
