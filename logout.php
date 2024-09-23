59e6708a71b911a4cb4a449a03c78f9397a2b9fb
Bill MacKenty <bmackenty@users.noreply.github.com>
2024-09-23 11:32:41 +0200

    Update logout.php

--- logout.php
+++ logout.php
@@ -11,21 +11,14 @@
         <h1>You Have Logged Out</h1>
     </header>
 
-    <nav>
-        <ul>
-            <li><a href="index.html">Home</a></li>
-            <li><a href="login.html">Login</a></li>
-            <li><a href="register.html">Register</a></li>
-        </ul>
-    </nav>
+<?php include('navbar.php'); ?>
 
     <main>
         <h2>Thank You for Visiting</h2>
         <p>You have successfully logged out. If you wish to log in again, please click <a href="login.html">here</a>.</p>
     </main>
 
-    <footer>
-        <p>&copy; 2024 Your Website</p>
-    </footer>
+<?php include('footer.php'); ?>
+
 </body>
 </html>
