a1bccaefd08b4436f2e460a198a9810c613a06cf
Bill MacKenty <bmackenty@users.noreply.github.com>
2024-09-26 07:27:38 +0200

    Update list_users.php

--- list_users.php
+++ list_users.php
@@ -30,27 +30,47 @@
             </thead>
             <tbody>
 
+<?php
+    // This line includes an external PHP file 'database_inc.php'
+    // which contains the database connection details such as the hostname, username, password, and database name.
+    // It ensures the database connection is available here.
+    include('database_inc.php'); 
 
-                <?php
+    // This line sends a SQL query to the database to retrieve all rows from the 'users' table.
+    // The 'mysqli_query()' function takes two parameters: the database connection and the SQL query.
+    $result = mysqli_query(
+        $connect, // Database connection object provided by 'database_inc.php'
+        "SELECT * FROM users;" // SQL query: Select all records from the 'users' table
+    );
 
-                include('database_inc.php');
+    // This is a loop that iterates through the result set (the rows returned by the SQL query).
+    // The 'mysqli_fetch_array()' function fetches one row at a time from the result set.
+    // Each row is returned as an associative array, with column names as keys.
+    while ($row = mysqli_fetch_array($result)) {
+?>
 
-                $result = mysqli_query(
-                    $connect,
-                    "SELECT * FROM users;"
-                );
+    <!-- The HTML part below will generate a new table row for each user in the result set -->
+    <tr>
+        <!-- The 'scope' attribute is used for accessibility to describe the headers in the table -->
+        <!-- This line outputs the 'id' column from the current row in the result set -->
+        <th scope="row"><?php echo $row['id']; ?></th>
 
-                while ($row = mysqli_fetch_array($result)) {
-                ?>
-                    <tr>
-                        <th scope="row"><?php echo $row['id']; ?></th>
-                        <td><?php echo $row['email']; ?></td>
-                        <td><?php echo $row['password']; ?></td>
-                        <td>Edit this user</td>
-                    </tr>
-                <?php
-                }
-                ?>
+        <!-- This line outputs the 'email' column from the current row in the result set -->
+        <td><?php echo $row['email']; ?></td>
+
+        <!-- This line outputs the 'password' column (which is expected to be encrypted/hashed) from the current row -->
+        <td><?php echo $row['password']; ?></td>
+
+        <!-- This static text provides an action (in this case, "Edit this user") -->
+        <!-- You might later replace this with a button or link to edit the user -->
+        <td>Edit this user</td>
+    </tr>
+
+<?php
+    // End of the loop. It will continue iterating until all rows in the result set are processed.
+    }
+?>
+
             </tbody>
         </table>
 
