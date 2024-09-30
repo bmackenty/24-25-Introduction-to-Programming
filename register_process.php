f571e90c587a029b99fc4da24036bce3b1508943
Bill MacKenty <bmackenty@users.noreply.github.com>
2024-09-30 12:47:31 +0200

    Update register_process.php

--- register_process.php
+++ register_process.php
@@ -2,12 +2,38 @@
 
 include('database_inc.php');
 
-$username = $_POST['username'];
-$email = $_POST['email'];
-$password = $_POST['password'];
-$confirm_password = $_POST['confirm_password'];
+// Capture the data from the POST request, which is submitted via a form.
+// 'username', 'email', 'password', and 'confirm_password' are expected as input field names in the form.
+
+// Storing the username entered by the user.
+$username = $_POST['username']; 
+
+// Storing the email entered by the user.
+$email = $_POST['email']; 
+
+// Storing the password entered by the user.
+$password = $_POST['password']; 
+
+// Storing the confirmation password entered by the user.
+$confirm_password = $_POST['confirm_password']; 
+
+// for now we will assume every user is active, but if we want to change this to "pending" later we can. 
 $status = "active";
 
-$insert_query = "INSERT INTO users (username, email, password, status) VALUES ('$username', '$email', '$password', '$status');";
+// Verify if the password and confirm password fields match
+if ($password !== $confirm_password) {
+    // If passwords do not match, terminate script execution and return an error message.
+    die('Passwords do not match. Please go back and try again.');
+}
+
+
+// Hash the password using PHP's password_hash() function.
+// The PASSWORD_DEFAULT constant uses the bcrypt algorithm by default.
+// This ensures that the password is securely hashed before being stored in the database.
+
+$hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+
+$insert_query = "INSERT INTO users (username, email, password, status) VALUES ('$username', '$email', '$hased_password', '$status');";
 $sql_result = mysqli_query($connect, $insert_query);
  ?>
