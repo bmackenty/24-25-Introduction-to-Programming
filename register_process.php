7ac3e4935853dfa9d099d6bc83fe639cf0d971a9
Bill MacKenty <bmackenty@users.noreply.github.com>
2024-09-30 09:29:44 +0200

    Update register_process.php

--- register_process.php
+++ register_process.php
@@ -1,5 +1,7 @@
 <?php 
 
+include('database_inc.php');
+
 $username = $_POST['username'];
 $email = $_POST['email'];
 $password = $_POST['password'];
@@ -7,5 +9,4 @@ $confirm_password = $_POST['confirm_password'];
 
 $insert_query = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$password');";
 $sql_result = mysqli_query($connect, $insert_query);
-
-?>
+ ?>
